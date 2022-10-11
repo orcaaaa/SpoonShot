d88b2fd9a89503c660e00ab14029bd18eef59007
orcaaaa <sathyan.two@gmail.com>
2022-10-11 18:24:23 +0530

    added ingredients and modified foodranking.py

--- foodranking.py
+++ foodranking.py
@@ -1,11 +1,22 @@
-#fname = input('enter file name:')
-#if len(fname)<1:
- #   fname='foodarticle1.txt'
+fname = input('enter file name:')
+if len(fname)<1:
+  fname='foodarticle1.txt'
     
 
+#hand = open(fname,'r')
+# for w in hand:
+#   w = w.split()
+#   print(w)
+import nltk 
+from nltk import word_tokenize as wt
+from nltk.corpus import stopwords
 with open('foodarticle1.txt') as f:
+    lines = f.readlines()
 
-    lines =f.readlines()
-    print(lines)
+sentences = ''.join(lines)
+tokens = wt(sentences)
 
-    
+tokens_without_sw = [word for word in tokens if not word in stopwords.words()]
+
+print(tokens_without_sw)
+
